6d51510f813dea10658501b842758bdb027427e4
gabey <gabey@localhost>
2025-08-25 18:04:28 -0400

    This is assignment 1

--- LSP__Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP__Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,7 +1,10 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+    /**
+     * Peter Collins
+     * @param args
+     */
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 
