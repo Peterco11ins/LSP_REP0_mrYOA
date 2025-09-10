7e2df749a698d343a3a1daeb6c2249d733c818ae
PeterCollins <Peterco11ins@users.noreply.github.com>
2025-09-10 11:23:38 -0400

    Remove old assignment1/assignment2 sources and classes

--- LSP__Project/src/org/howard/edu/lsp/assignment1/assignment2/Main.java
+++
@@ -1,8 +0,0 @@
-package org.howard.edu.lsp.assignment2;
-
-public class Main {
-    public static void main(String[] args) {
-        ETLProcessor processor = new ETLProcessor();
-        processor.processETL();
-    }
-} 
